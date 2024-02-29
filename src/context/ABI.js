c10a8a5a129475fd6244f4347824d5f3bf3111a4
RISCode1010 <92925907+RISCode1010@users.noreply.github.com>
2024-03-01 02:15:01 +0530

    otherUpdate

--- src/context/ABI.js
+++ src/context/ABI.js
@@ -1,5 +1,4 @@
 import ABI from './NFTMarketplace.json';
-// import ABI from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
 
-export const ContractAddress = '0x51F590d345A9862Ed1520134E163aFa08D860415';
+export const ContractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 export const ContractAddressABI = ABI.abi;
